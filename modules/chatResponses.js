81b83eea5758a5add4ce7a2b9623ff2bd2bf1024
KimpiegamesYT <broek.floris@gmail.com>
2026-03-24 11:38:31 +0100

    Add new chat trigger for 'iso' with corresponding response

--- modules/chatResponses.js
+++ modules/chatResponses.js
@@ -40,6 +40,7 @@ const chatTriggers = [
   { trigger: 'wifi', response: 'https://www.youtube.com/watch?v=FfUBYBuSog8', exactMatch: true },
   { trigger: 'tom', response: 'https://tommyteleshopping.com/', exactMatch: true },
   { trigger: 'nick', response: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.thedailymeal.com%2Fimg%2Fgallery%2F10-things-you-didnt-know-about-snickers%2FGettyImages-511842692_1_.jpg&f=1&nofb=1&ipt=6b558055cad825634767e54c191dc6d9f482e7f08f6868394eabae024b4146d1', exactMatch: true },
+  { trigger: 'iso', response: 'https://os.click/en', exactMatch: true },
   { trigger: 'stijn', response: 'Stijn Barendse, faalt steeds in de theorie!', exactMatch: true, audioFile: 'AI stijn.mp3' }
 ];
 
